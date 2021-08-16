b1841b2f3d26a15db070c3637cdf884633410a59
Rukonju <rukonbds@gmail.com>
2021-08-16 17:25:53 +0600

    final

--- js/account.js
+++ js/account.js
@@ -1,33 +1,29 @@
-function getInputValue(){
-    const depositInput=document.getElementById("deposit");
-    const depositValue=depositInput.value;
-    const depositAmount=parseFloat (depositValue);
-    depositInput.value='';
-    return depositAmount;
-    
+function getInputValue(inputId){
+    const input=document.getElementById(inputId);
+    const value=input.value;
+    const amount=parseFloat (value);
+    input.value='';
+    return amount;  
 }
+/* function totalBalance(){
+
+} */
 document.getElementById("deposit-btn").addEventListener('click',function() {
-    const depositAmount=getInputValue();
+    const depositAmount=getInputValue("deposit");
     const inialDeposit=document.getElementById("total-deposit");
     const initialDepositAmount=parseFloat(inialDeposit.innerText);
     inialDeposit.innerText=initialDepositAmount+depositAmount;
     const  initialBalance=document.getElementById("total");
     const  initialBalanceValue=parseFloat(initialBalance.innerText);
-    initialBalance.innerText=depositAmount+initialBalanceValue;
-
-    
+    initialBalance.innerText=depositAmount+initialBalanceValue;  
 });
 //withdraw
 document.getElementById("withdraw-btn").addEventListener('click',function() {
-    const withdrawInput=document.getElementById("withdraw");
-    const withdrawValue=withdrawInput.value;
-    const withdrawAmount=document.getElementById('total-withdraw');
-    let totalWithdraw=withdrawAmount.innerText;
-    totalWithdraw=parseFloat (withdrawValue) + parseFloat (totalWithdraw);
-    withdrawAmount.innerText=totalWithdraw;
-    withdrawInput.value='';
-    const balance=document.getElementById("total");
-    let totalBalance=balance.innerText;
-    totalBalance=parseFloat(totalBalance)- parseFloat(totalWithdraw);
-    balance.innerText=totalBalance;
+    const withdrawAmount=getInputValue("withdraw");
+    const initialWithdraw=document.getElementById('total-withdraw');
+    const initialWithdrawValue=parseFloat(initialWithdraw.innerText);
+    initialWithdraw.innerText= initialWithdrawValue+withdrawAmount;
+    const initialBalance=document.getElementById("total");
+    const initialBalanceValue=parseFloat(initialBalance.innerText);
+    initialBalance.innerText=initialBalanceValue-withdrawAmount;
 });
