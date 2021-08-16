68e9f0fefa32b78b879b7317c4995bd48bf891a2
Rukonju <rukonbds@gmail.com>
2021-08-16 21:39:00 +0600

    final

--- js/account.js
+++ js/account.js
@@ -6,23 +6,46 @@ function getInputValue(inputId){
     return amount;  
 }
 function updateTranxiction(fieldId, depositAmount){
-    debugger;
     const tranxiction=document.getElementById(fieldId);
     const tranxictionAmount=parseFloat(tranxiction.innerText);
     tranxiction.innerText=tranxictionAmount+depositAmount;
 }
-document.getElementById("deposit-btn").addEventListener('click',function() {
-    const depositAmount=getInputValue("deposit");
-    updateTranxiction("total-deposit",depositAmount);
+function currentBalance(){
     const  initialBalance=document.getElementById("total");
     const  initialBalanceValue=parseFloat(initialBalance.innerText);
-    initialBalance.innerText=depositAmount+initialBalanceValue;  
+    return initialBalanceValue;
+}
+function totalbalance(depositAmount,isAdd){
+    const  initialBalance=document.getElementById("total");
+    // const  initialBalanceValue=parseFloat(initialBalance.innerText);
+    const  initialBalanceValue=currentBalance();
+    if(isAdd==true){
+        initialBalance.innerText=initialBalanceValue + depositAmount;
+    }
+    else{
+        initialBalance.innerText=initialBalanceValue - depositAmount;
+    }
+    
+}
+document.getElementById("deposit-btn").addEventListener('click',function() {
+    const depositAmount=getInputValue("deposit");
+    if (depositAmount>0){
+        updateTranxiction("total-deposit",depositAmount); 
+        totalbalance(depositAmount,true);
+    }
+    
 });
 //withdraw
 document.getElementById("withdraw-btn").addEventListener('click',function() {
     const withdrawAmount=getInputValue("withdraw");
-    updateTranxiction('total-withdraw',withdrawAmount);
-    const initialBalance=document.getElementById("total");
-    const initialBalanceValue=parseFloat(initialBalance.innerText);
-    initialBalance.innerText=initialBalanceValue-withdrawAmount;
+    const balance=currentBalance();
+    if (withdrawAmount>0 && withdrawAmount<balance){
+        updateTranxiction('total-withdraw',withdrawAmount);
+        totalbalance(withdrawAmount,false);
+    }
+    else{
+        alert("insufficient balance");
+       
+    }
+    
 });
